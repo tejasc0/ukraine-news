dfc1351d21c5ac9518a61159390de3d3b603ae91
tejasc0 <tejasc888@gmail.com>
2022-04-16 11:49:03 +0000

    added ukraine news

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
 import './App.css'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const options = {
+    method: 'GET',
+    url: 'https://free-news.p.rapidapi.com/v1/search',
+    params: { q: 'ukraine', lang: 'en' },
+    headers: {
+      'X-RapidAPI-Host': 'free-news.p.rapidapi.com',
+      'X-RapidAPI-Key': import.meta.env.VITE_API_KEY
+    }
+  };
+  useEffect(() => {
+    axios.request(options).then(function (response) {
+      setLoading(false);
+      setData(response.data.articles);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }, []);
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="text-sky-800 md:text-6xl text-3xl md:m-10 m-4 font-thin">Ukraine News</div>
+
+      {loading && <div className="text-2xl mx-auto w-max md:my-10 my-4">Loading...</div>}
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 mx-auto p-4">
+
+        {data.map((item) => {
+          return <div key={item._id}>
+            <a href={item.link} target="_blank">
+              <div className="shadow-lg border-2 m-4 p-3 rounded-lg">
+                <img src={item.media} alt="" />
+                <div className="">{item.title}</div>
+              </div></a>
+
+          </div>
+        })}
+      </div>
     </>
   )
 }
